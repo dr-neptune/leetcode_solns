5a4d94b95b8f3c622c7e4fd2f2193632771ec08d
dr-neptune <mrose4@worcester.edu>
2022-01-13 12:52:17 -0500

    binomial coefficient

--- pascals_triangle.rkt
+++ pascals_triangle.rkt
@@ -1,8 +1,53 @@
 #lang racket
 (require racket)
 
+;; without memoization
+(define (binomial-coef n k [i 1])
+    (if (= (add1 k) i)
+        1
+        (* (/ (- n (- k i)) i)
+           (binomial-coef n k (add1 i)))))
 
-;; idea
-;; use math
-;; maybe multiplicative formula for n choose k
-;; and then use dynamic programming and fill in each row up to numRows
+(define (binomial n k)
+  (if (< (- n k) k)
+      (binomial-coef n (- n k))
+      (binomial-coef n k)))
+
+(define (generate-row row)
+  (map (lambda (k) (binomial row k))
+         (range 0 (add1 row))))
+
+(define (generate numRows [k 0])
+  (if (= numRows k)
+      '()
+      (cons (generate-row k)
+            (generate numRows (add1 k)))))
+
+;; with memoization (this is actually much slower!)
+(define (memoize fn)
+  (let ((cache (make-hash)))
+    (lambda arg
+      (hash-ref! cache arg (thunk (apply fn arg))))))
+
+(define (binomial-coef n k [i 1])
+    (if (= (add1 k) i)
+        1
+        (* (/ (- n (- k i)) i)
+           (binomial-coef n k (add1 i)))))
+
+(define binomial
+  (memoize
+   (lambda (n k)
+     (if (< (- n k) k)
+         (binomial-coef n (- n k))
+         (binomial-coef n k)))))
+
+(define (generate-row row)
+  (map (lambda (k) (binomial row k))
+         (range 0 (add1 row))))
+
+(define (generate numRows [k 0])
+  (if (= numRows k)
+      '()
+      (cons (generate-row k)
+            (generate numRows (add1 k)))))
