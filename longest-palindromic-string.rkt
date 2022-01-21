2ef6362b97a35916da9954da16eaa24fefbb3100
dr-neptune <mrose4@worcester.edu>
2022-01-21 09:00:23 -0500

    now to speed it up

--- longest-palindromic-string.rkt
+++ longest-palindromic-string.rkt
@@ -3,56 +3,34 @@
 
 (define exstr1 "babad")  ;; bab
 (define exstr2 "cbbd")   ;; bb
+(define exstr3 "aacabdkacaa") ;; aca
 
-;; idea
-;; brute force
-;; get every combination of the letters and return only those that are palindromes
-;; then find the longest one
 
-(map string (combinations (string->list exstr1)))
+(define (sliding-window ls n [vals '()])
+  (if (= n (length ls))
+      (reverse (cons ls vals))
+      (sliding-window (rest ls) n (cons (take ls n) vals))))
 
-;; idea
-;; stream the combinations
-;; check each for palindrome
-;; keep a max size palindrome
+
+(define (all-slides ls [k 0] [vals '()])
+  (if (= k (length ls))
+      vals
+      (all-slides ls (add1 k) (append vals (sliding-window ls (add1 k))))))
+
 
 (define (is-palindrome ls)
   (equal? ls (reverse ls)))
 
-(define largest '(0 0))
-
-(for ([val (in-combinations (string->list exstr1))])
-  (when (is-palindrome val)
-    (when (> (length val) (first largest))
-      (set! largest (list (length val) val))))
-  (println val))
-
 
 (define (longest-palindrome s)
   (let ([largest '(0 0)])
-    (for ([val (in-permutations (string->list s))])
+    (for ([val (in-list (all-slides (string->list s)))])
       (when (is-palindrome val)
         (when (> (length val) (first largest))
           (set! largest (list (length val) val)))))
     (list->string (second largest))))
 
+
 (longest-palindrome exstr1)
-
 (longest-palindrome exstr2)
-
-
-(define exstr3 "aacabdkacaa")
-
 (longest-palindrome exstr3)
-
-
-;; in-list coerces the list to a stream for use with a for statement
-(in-list '(1 2 3))
-
-;; write a function that gets a sliding window of every size over the string
-;; string
-;; strin tring
-;; stri trin ring
-;; str tri rin ing
-;; st tr ri in ng
-;; s t r i n g
