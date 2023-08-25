517b31dbae9f25d3e19750ff9405af35cc33c8de
dr-neptune <mrose4@worcester.edu>
2023-08-25 15:18:13 -0400

    rebers

---
+++ reverse-vowels-of-a-string.rkt
@@ -0,0 +1,95 @@
+#lang racket
+(require racket)
+
+;; idea
+;; traverse once and collect all the vowels
+;; traverse a second time and replace values (take-right)
+(define (vowel? c)
+    (match c
+      [(or #\a #\e #\i #\o #\u
+           #\A #\E #\I #\O #\U) #t]
+      [_ #f]))
+
+(define (reverse-vowels s)
+  (let* ([chars (string->list s)]
+         [vowels (for/list ([c chars]
+                            #:when (vowel? c))
+                   c)])
+    (let iter ([str chars]
+               [vs (reverse vowels)]
+               [acc '()])
+      (cond [(null? str) (list->string (reverse acc))]
+            [(vowel? (first str)) (iter (rest str)
+                                        (rest vs)
+                                        (cons (first vs) acc))]
+            [else
+             (iter (rest str) vs (cons (first str) acc))]))))
+
+
+(define (reverse-vowels s)
+  (let* ([chars (string->list s)]
+         [vowels (for/list ([c chars]
+                            #:when (vowel? c))
+                   c)])
+    (for/list ([s chars]
+               [k vowels]
+               #:when (vowel? s)
+               [k vowels])
+      ???)))
+
+
+(define chars (string->list "leetcode"))
+(define vowels (list #\e #\e #\o #\e))
+
+result: "leotcede"
+
+(for/fold ([s chars]
+           [v vowels]))
+
+
+
+(define (reverse-vowels str vowels)
+  (let* ([chars (string->list str)]
+         [reversed-vowels (reverse vowels)])
+    (for/fold ([new-str '()] [vowels reversed-vowels])
+              ([char chars])
+      (cons (if (member char '(#\a #\e #\i #\o #\u #\A #\E #\I #\O #\U))
+                  (begin (set! vowels (cdr vowels))
+                         (if vowels (car vowels) char))
+                  char)
+            new-str))))
+
+(reverse-vowels "leetcode" vowels)
+(define str "leetcode")
+(define vowels (list #\e #\e #\o #\e))
+(list->string (reverse (reverse-vowels str vowels)))
+
+(for/foldr ([acc '()])
+            ([s chars])
+            ;; ([v vowels])
+           (cons v acc))
+
+(define (in-printing seq)
+  (sequence-map (lambda (v) (println v) v) seq))
+
+(for/foldr ([acc '()])
+           ([v (in-printing (in-range 1 4))])
+  (println v)
+  (cons v acc))
+
+(for/foldr ([result '()])
+           ([s chars]
+            [v vowels])
+  (cons (if (vowel? s) v s) result))
+
+(define str "leetcode")
+(define vowels (list #\e #\e #\o #\e))
+
+(for/foldr ([result '()])
+           ([s chars]
+            [v vowels])
+  (cons (if (vowel? s) v s) result))
+
+(reverse-vowels "leetcode")
+(reverse-vowels "hello")
+(reverse-vowels "b")
