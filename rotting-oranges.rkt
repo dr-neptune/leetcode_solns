08e1eff69fab48c613afc453292d78f38d81a70f
dr-neptune <mrose4@worcester.edu>
2023-10-04 17:14:50 -0400

    push it along, yeah push it along

--- rotting-oranges.rkt
+++ rotting-oranges.rkt
@@ -19,15 +19,6 @@
 ;; scan for a rotten orange
 ;; start at a rotten orange
 
-;; get xs
-;; get ys
-
-;;
-
-(define exgrid '((2 1 1)
-                 (1 1 0)
-                 (0 1 1)))
-
 (define (grid->vec grid)
   (vector-map list->vector (list->vector grid)))
 
@@ -54,10 +45,97 @@
 
 ;; rework this to go clockwise starting from midnight
 ;; or use an alist to tag them
-(let ([x 1] [y 1])
-  (filter (compose not negative?)
-          (map (λ (ls) (apply (curry get-element (grid->vec exgrid)) ls))
-               (list (list (add1 x) y)
-                 (list (sub1 x) y)
-                 (list x (add1 y))
-                 (list x (sub1 y))))))
+(let ([x 0] [y 0])
+  (filter (compose not negative? cadr)
+          (map (λ (ls dir) (list dir (apply (curry get-element (grid->vec exgrid)) ls)))
+               (list
+                (list x (sub1 y))  ;; N
+                (list (add1 x) y)  ;; E
+                (list x (add1 y))  ;; S
+                (list (sub1 x) y)) ;; W
+               (list #\n #\e #\s #\w))))
+
+(define (get-adjacent grid x y)
+  (filter (compose not negative? cdr)
+          (map (λ (ls dir) (cons dir (apply (curry get-element grid) ls)))
+               (list
+                (list x (sub1 y))  ;; N
+                (list (add1 x) y)  ;; E
+                (list x (add1 y))  ;; S
+                (list (sub1 x) y)) ;; W
+               (list #\n #\e #\s #\w))))
+
+(define exadj (get-adjacent (grid->vec exgrid) 1 1))
+
+;; make a rot function
+;; have it take the output of get-adjacent
+;; if any of the values in the alists are 1
+;; set them to 2 and add 1 to the counter
+;; in order to avoid n^2 performance, use mutation
+
+(define (get-location dir x y)
+  (match dir
+    [#\n (list x (sub1 y))]
+    [#\e (list (add1 x) y)]
+    [#\s (list x (add1 y))]
+    [#\w (list (sub1 x) y)]))
+
+(map (λ (pair) (get-location (car pair) orig-x orig-y)) (filter (λ (p) (= 1 (cdr p))) exadj))
+
+(define grid (grid->vec exgrid))
+
+(define (rot adj-list orig-x orig-y)
+   (let ([rot-coords (map (λ (pair) (get-location (car pair) orig-x orig-y))
+                          (filter (λ (p) (= 1 (cdr p))) adj-list))])
+     (displayln (format "rot coords: ~a" rot-coords))
+     (for ([c rot-coords])
+       (apply (curry update-grid-position! grid 2) c)
+       (displayln (format "grid: ~a" grid)))))
+
+;; (rot exadj 1 1)
+
+(define (update-grid-position! grid v x y)
+  (when (and (not (negative? x))
+             (not (negative? y)))
+    (let ([tbm (vector-ref grid y)])
+      (vector-set! tbm x v)
+      (vector-set! grid y tbm))))
+
+(define (vector-ref-2 grid x y)
+  (vector-ref (vector-ref grid y) x))
+
+(define grid (grid->vec exgrid))
+
+(let ([num-mins 0])
+  (for* ([x (vector-length (vector-ref grid 0))]
+         [y (vector-length grid)])
+    (when (= 2 (vector-ref-2 grid x y))
+      (let* ([adj-grid (get-adjacent grid x y)]
+             [rot-coords (get-rot-coords adj-grid x y)])
+        (when (not (empty? rot-coords))
+            (set! num-mins (add1 num-mins))
+            (update-rot-coords! grid rot-coords)))))
+  num-mins)
+
+(rot (get-adjacent grid 0 0) 0 0)
+
+;; currently we are updating the grid
+;; but we need to count how many rounds it takes to change everything to 2
+;; and we can't double count for times when everything is already a 2
+
+;; idea
+;; update rot to return a 1 if anything is updated, 0 otherwise
+;; (define (rot adj-list orig-x orig-y)
+;;    (let ([rot-coords (map (λ (pair) (get-location (car pair) orig-x orig-y))
+;;                           (filter (λ (p) (= 1 (cdr p))) adj-list))])
+;;      (for ([c rot-coords])
+;;        (apply (curry update-grid-position! grid 2) c)
+;;        (displayln (format "grid: ~a" grid)))))
+
+(define (get-rot-coords adj-list orig-x orig-y)
+  (map (λ (pair) (get-location (car pair) orig-x orig-y))
+       (filter (λ (p) (= 1 (cdr p))) adj-list)))
+
+(define (update-rot-coords! grid coords [v 2])
+  (for ([c coords])
+       (apply (curry update-grid-position! grid v) c)))
