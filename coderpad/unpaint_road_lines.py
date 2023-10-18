b024626aeea7b6d4f911c92584d628cd48a667f9
dr-neptune <mrose4@worcester.edu>
2023-10-18 18:04:30 -0400

    workinonit

--- coderpad/unpaint_road_lines.py
+++ coderpad/unpaint_road_lines.py
@@ -136,23 +136,147 @@ for each start -> end iter, add 1 to our array
 
 """
 
+from itertools import groupby
+
+exin = (3, 10, 14, 20, 1, 5)
+
+pairs = []
+for i in range(0, len(exin), 2):
+    pair = (exin[i], exin[i+1])
+    pairs.append(pair)
+pairs = sorted(pairs)
+
 paint_counter = [0 for _ in range(1, max(pairs, key=lambda x: x[1])[1] + 1)]
 
 ls1, ls2 = map(sorted, zip(*pairs))
 
 for start, end in zip(ls1, ls2):
     for i in range(start-1, end):
-        print(i)
         paint_counter[i] += 1
 
-# now we want to partition our new array into chunks, split by 0
+partitions = [list(g) for k, g in groupby(paint_counter)]
 
-new_pairs = []
-min_element, max_element = 0, 0
-# consider indices that start at 0!
-for idx, element in enumerate(paint_counter):
-    if element != 0:
-        max_element = idx
+# agglomerate the partitions
+# then check if > 5 overlaps
+partitions = [[1, 1], [2, 2, 2], [1, 1, 1, 1, 1], [0, 0, 0], [1, 1, 1, 1, 1, 1, 1]]
+
+def combine_if_not_zero(a, b):
+    if a[0] != 0 and b[0] != 0:
+        return a + b
     else:
-        new_pairs.append((min_element, max_element))
-        min_element = idx
+        return a + [b]
+
+reduce(combine_if_not_zero, partitions)
+
+
+# counts = []
+# num_min, num_max = 1, 0
+# for ls in partitions:
+#     if ls[0] != 0 and ls[-1] != 0:
+#         num_max += len(ls)
+#     elif ls[0] == 0:
+#         counts.append([num_min, num_max])
+#         num_min = num_max + 1 + len(ls)
+#         num_max = num_min - 1
+
+# last_pair = [num_min, num_max]
+# if not last_pair in counts:
+#     counts.append(last_pair)
+
+# print(counts)
+
+# try tests
+
+# def analyze_paint_logs(logs):
+#     pairs = []
+#     for i in range(0, len(logs), 2):
+#         pair = (logs[i], logs[i+1])
+#         pairs.append(pair)
+#     pairs = sorted(pairs)
+
+#     paint_counter = [0 for _ in range(1, max(pairs, key=lambda x: x[1])[1] + 1)]
+
+#     ls1, ls2 = map(sorted, zip(*pairs))
+
+#     for start, end in zip(ls1, ls2):
+#         for i in range(start-1, end):
+#             paint_counter[i] += 1
+
+#     partitions = [list(g) for k, g in groupby(paint_counter)]
+
+#     counts = []
+#     num_min, num_max = 1, 0
+#     for ls in partitions:
+#         if ls[0] != 0 and ls[-1] != 0:
+#             num_max += len(ls)
+#         elif ls[0] == 0:
+#             counts.append([num_min, num_max])
+#             num_min = num_max + 1 + len(ls)
+#             num_max = num_min - 1
+
+#     last_pair = [num_min, num_max]
+#     if not last_pair in counts:
+#         counts.append(last_pair)
+
+#     return counts
+
+
+analyze_paint_logs(exin)
+
+
+def main():
+    unpaint_instructions = analyze_paint_logs((3, 10, 14, 20, 1, 5))
+    print(unpaint_instructions)
+    assert unpaint_instructions == [1, 1, 10, 1, 14, 20]
+
+    unpaint_instructions = analyze_paint_logs((1, 7, 1, 7, 1, 11, 1, 7, 1, 7))
+    print(unpaint_instructions)
+    assert unpaint_instructions == [5, 1, 7, 1, 7, 11]
+
+    unpaint_instructions = analyze_paint_logs(
+        (
+            5.2, 10.7, 5.3, 10.6, 5.0, 10.9, 5.1, 10.8,
+            7.7, 8.8, 6.6, 7.7, 7.0, 8.0,
+            1.5, 2.3, 1.6, 2.1, 2.3, 3.4,
+        )
+    )
+    print(unpaint_instructions)
+    assert unpaint_instructions == [
+        1, 1.5, 3.4,
+        1, 5.0, 6.6,
+        5, 6.6, 8.8,
+        1, 8.8, 10.9
+    ]
+
+main()
+
+
+
+# def analyze_paint_logs(coords):
+#     painted = [False for _ in range(max(coords) + 1)] # Add 1 to guarantee unpainted last cell
+
+#     # Naive painting algorithm
+#     for coord_i in range(0, len(coords), 2):
+#         start = coords[coord_i]
+#         end = coords[coord_i + 1]
+#         for paint_idx in range(start, end):
+#             painted[paint_idx] = True
+
+#     previous_painted = False
+#     interval_start = None
+#     result = []
+#     for i in range(len(painted)):
+#         if painted[i] and not previous_painted:
+#             # Found new interval start
+#             interval_start = i
+#         if not painted[i] and previous_painted:
+#             # Found current interval end
+#             result.extend([interval_start, i])
+
+#         # Remember previous cell state
+#         previous_painted = painted[i]
+
+#     return tuple(result)
+
+
+# analyze_paint_logs(exin)
